a8c49aec08519c7982a95388bef7e5ad4c237ef1
HinderingIce <145399087+HinderingIce@users.noreply.github.com>
2025-10-21 18:22:13 +0000

    Commit

--- Main.java
+++ Main.java
@@ -4,6 +4,40 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    //  for(int i; i <= 25; i++)
+    //  {
+    //     System.out.println(i);
+    //  }
+
+    // for(int i = 17; i <= 73;)
+    // {
+    //   for(int e = 1; e <= 10 && i <= 73; e++)
+    //   {
+    //     System.out.print(i + " ");
+    //     i++;
+    //   }
+    //   System.out.println();
+    // }
+    int a =sc.nextInt();
+
+    if (a < 0)
+    {
+      System.out.println("error");
+    }
+    else
+    {
+      for(int i = a; i <= 50;)
+    {
+      for(int e = 1; e <= 5 && i <= 50; e++)
+      {
+        System.out.print(i + " ");
+        i++;
+      }
+      System.out.println();
+    }
+    }
+
+    sc.close();
   }
 }
